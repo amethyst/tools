efd580b3bd47db664a87fc7d255ff96eeb5bc0d2
Eyal Kalderon <ebkalderon@gmail.com>
2016-01-05 20:44:06 -0500

    enable arbitrary arguments in execute! macro

--- src/flag_version.rs
+++ src/flag_version.rs
@@ -3,7 +3,7 @@
 const VERSION: &'static str = env!("CARGO_PKG_VERSION");
 
 /// Prints crate version information.
-pub fn execute(_args: &Vec<String>) -> Result<(), &'static str> {
+pub fn execute() -> Result<(), &'static str> {
     println!("amethyst_cli {}",  VERSION);
     Ok(())
 }
