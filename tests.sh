6f55c06b24e8499c2f38137ba61f8e70b598c71a
Eyal Kalderon <ebkalderon@gmail.com>
2016-01-07 00:22:39 -0500

    add integration tests, tweak .travis.yml

---
+++ tests.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+# Perform basic integration testing on amethyst_cli.
+
+function check_new() {
+    echo "--- amethyst new"
+
+    ln -s ./target/debug/amethyst .
+    ./amethyst new mygame
+
+    if [ -f mygame/Cargo.toml ] &&
+       [ -d mygame/resources/entities/ ] &&
+       [ -d mygame/resources/prefabs/ ] &&
+       [ -f mygame/resources/config.yml ] &&
+       [ -f mygame/resources/input.yml ] &&
+       [ -f mygame/src/main.rs ]; then
+        echo "--- Passed!"
+	echo
+	return
+    fi
+
+    ls -l mygame
+    exit 1
+}
+
+function check_build() {
+    echo "--- amethyst build"
+
+    cd mygame
+    ../amethyst build
+
+    if [ $? -eq 0 ]; then
+        echo "--- Passed!"
+	echo
+        return
+    fi
+
+    ls -l
+    exit 1
+}
+
+function check_run() {
+    echo "--- amethyst run"
+
+    ../amethyst run
+
+    if [ $? -eq 0 ]; then
+        echo "--- Passed!"
+	echo
+        return
+    fi
+
+    ls -l
+    exit 1
+}
+
+function check_clean() {
+    echo "--- amethyst clean"
+
+    ../amethyst clean
+
+    if [ ! -d target ]; then
+        echo "--- Passed!"
+	echo
+	return
+    fi
+
+    ls -l
+    exit 1
+}
+
+check_new
+check_build
+check_run
+check_clean
+
+echo
+echo "All tests pass!"
+cd ..
+rm -r amethyst mygame
