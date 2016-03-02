b2bf0ed902f24fa5960be2c977f4827b56c8ade7
Oliver Goding <olivergoding@gmail.com>
2016-03-02 17:41:54 +1100

    Added compression of resources folder. Currently just compresses all files within the folder using the Deflate compression method. Improved the error handling but some work converting std::io::Error to &str still needs to happen.

--- src/cli/subcmds/deploy.rs
+++ src/cli/subcmds/deploy.rs
@@ -5,10 +5,11 @@ use clap::ArgMatches;
 use cargo;
 
 use std::fs;
-use std::io::{Read, Write, ErrorKind};
+use std::io::{Read, Write, Error, ErrorKind};
 use std::path::Path;
-//use zip::{ZipWriter, CompressionMethod};
+use zip::{ZipWriter, CompressionMethod};
 
+/// Print all files in resources directory. Currently only being used for debugging
 fn list_files() {
     let paths = fs::read_dir("resources").unwrap();
 
@@ -20,59 +21,79 @@ fn list_files() {
 const DEPLOY_DIR: &'static str = "deployed";
 
 /// Create a deployment directory
-// TODO Add in result return type so that function/command can fail
-fn setup_deploy_dir() {
+fn setup_deploy_dir() -> Result<(), Error> {
     fs::create_dir(DEPLOY_DIR).or_else(|e| match e.kind() {
         ErrorKind::AlreadyExists => Ok(()),
         _ => Err(e),
     });
 
-    let deploy_dir = Path::new(DEPLOY_DIR);
     // Clean out any existing files that have been deployed.
-    for path in fs::read_dir(deploy_dir).unwrap() {
-        // TODO wrap the remove file in a try! macro
-        fs::remove_file(path.unwrap().path().as_path());
+    for path in fs::read_dir(Path::new(DEPLOY_DIR)).unwrap() {
+        try!(fs::remove_file(path.unwrap().path().as_path()));
     }
+    
+    Ok(())
 }
 
 /// Compress a directory and all of it's files
-// TODO Add in result return type so that function/command can fail
-// FIXME put try! macro around io
-fn zip_dir<P: AsRef<Path>>(dir: P, target_file: P) {
-    // TODO Implement compression/fix errors
-    /*let zip_file = fs::File::create(&target_file).unwrap();
+fn zip_dir<P: AsRef<Path>>(dir: P, target_file: P) -> Result<(), Error> {
+    let zip_file = fs::File::create(&target_file).unwrap();
     let mut zip = ZipWriter::new(zip_file);
-    zip.start_file(dir, CompressionMethod::Deflated);
+    //try!(zip.start_file(dir, CompressionMethod::Deflated));
+    // FIXME Make this use dir parameter
+    try!(zip.start_file("resources", CompressionMethod::Deflated));
 
     for path in fs::read_dir(dir).unwrap() {
         let file_path = path.unwrap();
         let mut file = fs::File::open(&file_path.path().as_path()).unwrap();
-        zip.start_file(file_path.path().file_name().unwrap().to_str().unwrap(), CompressionMethod::Deflated);
+        try!(zip.start_file(file_path.path().file_name().unwrap().to_str().unwrap(), CompressionMethod::Deflated));
 
         let mut file_body = String::new();
-        file.read_to_string(&mut file_body);
-        zip.write_all(file_body.as_bytes());
+        try!(file.read_to_string(&mut file_body));
+        try!(zip.write_all(file_body.as_bytes()));
     } 
 
-    zip.finish();*/
+    try!(zip.finish());
+    
+    Ok(())
 }
 
 /// Compresses and deploys the project as a distributable program.
 pub fn execute(_matches: &ArgMatches) -> cargo::CmdResult {
-    // TODO use new ? syntax or some other method of chaining these commands.
     try!(::subcmds::clean::execute(_matches));
     try!(::subcmds::test::execute(_matches));
     match ::subcmds::build::execute(_matches) {
         Ok(a) => {
             list_files();
-            setup_deploy_dir();
-            // FIXME wrap function call in try!
+            // FIXME Work out way to conver std::io::Error to &str
+            // TODO simplify this repeated code
+            match setup_deploy_dir() {
+                Ok(()) => (),
+                Err(e) => {
+                    println!("{}", e);
+                    return Err(&stringify!(e));
+                },
+            };
+            
             // FIXME Change format! macro to something better
-            zip_dir("resources", &format!("{}/{}", DEPLOY_DIR, "resources.zip"));
-            // FIXME wrap function call in try!
-            // TODO Implement way of copying correct binary
-            //fs::copy("target/release/filename", &format!("{}/{}", DEPLOY_DIR, "filename"));
-
+            match zip_dir("resources", &format!("{}/{}", DEPLOY_DIR, "resources.zip")) {
+                Ok(()) => (),
+                Err(e) => {
+                    println!("{}", e);
+                    return Err(&stringify!(e));
+                },
+            };
+            
+            // TODO Implement method of getting correct binary name
+            match fs::copy("target/release/filename", &format!("{}/{}", DEPLOY_DIR, "filename")) {
+                // function returns an unsigned int code. 
+                Ok(a) => (),
+                Err(e) => {
+                    println!("{}", e);
+                    return Err(&stringify!(e));
+                },
+            };
+            
             Ok(a)
         },
         Err(e) => Err(e),
