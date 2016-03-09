b69377515fac9155c675772f515c7bcfb3ceb6fc
Lucio Franco <lucio.franco@du.edu>
2016-03-09 13:23:25 -0700

    added support for --release flag for build sub command

--- src/cli/subcmds/build.rs
+++ src/cli/subcmds/build.rs
@@ -5,6 +5,12 @@ use clap::ArgMatches;
 use cargo;
 
 /// Compiles the current Amethyst project.
-pub fn execute(_matches: &ArgMatches) -> cargo::CmdResult {
-    cargo::call(vec!["build", "--color=always"])
+pub fn execute(matches: &ArgMatches) -> cargo::CmdResult {
+    let mut args = vec!["build", "--color=always"];
+
+    if matches.is_present("release") {
+        args.push("--release");
+    }
+
+    cargo::call(args)
 }
