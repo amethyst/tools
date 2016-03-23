2b8f597a156130a045d425f5e1b20f233ec15ceb
White-Oak <lpzhelud@gmail.com>
2016-03-23 23:14:14 +0300

    Added function to check if the current project is amethyst project

--- src/cli/subcmds/clean.rs
+++ src/cli/subcmds/clean.rs
@@ -3,11 +3,13 @@
 use cargo;
 
 use super::amethyst_args::{AmethystCmd, AmethystArgs};
+use super::is_amethyst_project;
 pub struct Cmd;
 
 impl AmethystCmd for Cmd {
     /// Removes the target directory.
     fn execute<I: AmethystArgs>(matches: &I) -> cargo::CmdResult {
+        try!(is_amethyst_project());
         let mut args = vec!["clean", "--color=always"];
 
         if matches.is_present("release") {
--- src/cli/subcmds/mod.rs
+++ src/cli/subcmds/mod.rs
@@ -5,3 +5,14 @@ pub mod deploy;
 pub mod module;
 pub mod new;
 pub mod run;
+
+use cargo;
+use std::path::Path;
+pub fn is_amethyst_project() -> cargo::CmdResult {
+    let config_path = Path::new(&".").join("resources").join("config.yml");
+    if config_path.exists() {
+        Ok(())
+    } else {
+        Err("The specified project is not an amethyst project.")
+    }
+}
