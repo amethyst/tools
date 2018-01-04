c3441409361f35d4f277dfb6f6c6e9556228e2c8
Sam Rhody <samuel.rhody@jhuapl.edu>
2018-01-04 14:49:07 -0500

    Modifying to read in all files in the templates directory. These are now read into a map with the key in the form of v{MAJOR}.{MINOR}

--- build.rs
+++ build.rs
@@ -1,9 +1,10 @@
 extern crate ron;
 
-use std::fs::{File, read_dir};
-use std::io::Write;
-use std::path::{PathBuf, Path};
+use std::collections::HashMap;
 use std::env;
+use std::fs::{read_dir, File};
+use std::io::Write;
+use std::path::{Path, PathBuf};
 
 use ron::de::from_reader;
 
@@ -15,46 +16,34 @@ fn read_template_index<P: AsRef<Path>>(p: P) -> Vec<String> {
     let mut path = PathBuf::new();
     path.push(p);
     path.push("index.ron");
-    println!("{:?}", path);
     from_reader(File::open(&path).expect("Failed to open index.ron"))
         .expect("Failed to parse template index")
 }
 
 fn main() {
     let f = PathBuf::from(path("CARGO_MANIFEST_DIR", "templates"));
-    let mut indices = Vec::new();
+    let mut indices = HashMap::new();
     for version in read_dir(&f).unwrap() {
         let v = version.unwrap();
         let version_str = v.file_name().into_string().unwrap();
         let index = read_template_index(v.path());
-        println!("{:?}", index);
-        indices.push(index.into_iter().map(|s| {
-            let mut t = version_str.clone();
-            t.push('/');
-            t.push_str(&s);
-            t
-        }).collect::<Vec<String>>());
-        println!("{:?}", indices);         
+        indices.insert(version_str, index);
     }
 
-
-    let mut source_code = indices.iter().flat_map(|v| v.iter()).fold(
-        "pub fn template_files() -> Vec<(&'static str, &'static str)> {\n
-    let mut map = Vec::new();\n"
-                .to_owned(),
-            |s, file| {
-                format!(
-                    "{}     \
-                    map.push(({:?}, include_str!(concat!(env!(\"CARGO_MANIFEST_DIR\"),\
-                    concat!(\"/templates/\", {:?})))));\n",
-                    s,
-                    file,
-                    file
-                )
-            },
-        );
+    let mut source_code = String::from("use std::collections::HashMap;\n\npub fn template_files() -> HashMap<&'static str, Vec<(&'static str, &'static str)>> {
+    let mut map = HashMap::new();\n");
+    for (version, index) in indices.iter() {
+        source_code.push_str(&format!("    map.insert({:?}, ", version));
+        source_code.push_str(&index.iter().fold("vec![".to_owned(), |s, file| {
+            format!(
+                "{}({:?}, include_str!(concat!(env!(\"CARGO_MANIFEST_DIR\"),\
+                 concat!(\"/templates/\", concat!(concat!({:?}, \"/\"), {:?}))))), ",
+                s, file, version, file,
+            )
+        }));
+        source_code.push_str("]);\n")
+    }
     source_code += "    map\n}\n";
-    println!("{}", source_code);
     File::create(&path("OUT_DIR", "_template_files.rs"))
         .expect("Failed to embed template files")
         .write_all(source_code.as_bytes())
